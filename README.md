06ecd3ee25240b1ae9661094c8e3da5ee1cd20f6
GectoR <dmitry.pustowoy@yandex.ru>
2021-03-19 22:00:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Readable: A Reddit clone
+
+## Getting Started
+
+> - `npm install`
+> - `npm start`
+
+> - `cd server`
+> - `npm install`
+> - To start the server:
+> - `node server.js`
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
+import "semantic-ui-css/semantic.min.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from "redux-logger";
+import reducer from "./redux/reducers";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./themes";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
